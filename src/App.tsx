dd4373df7085680194c628de54724451e32f22bf
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2021-06-12 19:52:48 +0900

    update: props設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import List from './components/List';
 
 interface IState {
   people: {
@@ -17,6 +17,7 @@ function App() {
   return (
     <div className="App">
       <h1>People invited to my Party</h1>
+      <List people={people} />
     </div>
   );
 }
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,1 +1,18 @@
-//
+interface IProps {
+  people: {
+    name: string;
+    age: number;
+    url: string;
+    note?: string;
+  }[];
+}
+
+const List = (props: IProps) => {
+  return (
+    <div>
+      <p>I am list</p>
+    </div>
+  );
+};
+
+export default List;
