2f58c8433d65006ae3cc2f6fd3cbb07846c4fcf2
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2021-06-12 20:18:01 +0900

    update: ListコンポーネントJSX UI作成

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -7,12 +7,23 @@ interface IProps {
   }[];
 }
 
-const List = (props: IProps) => {
-  return (
-    <div>
-      <p>I am list</p>
-    </div>
-  );
+const List: React.VFC<IProps> = ({ people }) => {
+  const renderList = (): JSX.Element[] => {
+    return people.map((person) => {
+      return (
+        <li className="List">
+          <div className="List-header">
+            <img className="List-img" src={person.url} alt={person.name} />
+            <h2>{person.name}</h2>
+          </div>
+          <p>{person.age} years old</p>
+          <p className="List-note">{person.note}</p>
+        </li>
+      );
+    });
+  };
+
+  return <ul>{renderList()}</ul>;
 };
 
 export default List;
